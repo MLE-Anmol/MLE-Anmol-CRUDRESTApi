3655a527dd96a2910d4a3cd4188b825f8fd82061
MLE-Anmol <anmoluk1988@gmail.com>
2021-12-26 01:25:58 +0530

    App n models created

--- crudrest/crudrest/settings.py
+++ crudrest/crudrest/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'studentapi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'crudrest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'riscwqgj',
+        'USER': 'riscwqgj',
+        'PASSWORD': 'N453jumv9IsFL-2g_qkOUiJiMGpy_3xk',
+        'HOST': 'castor.db.elephantsql.com',
+        
     }
 }
 
--- crudrest/studentapi/models.py
+++ crudrest/studentapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    stu_name = models.CharField(max_length=20)
+    stu_id = models.CharField(max_length=10)
+    Course = models.CharField(max_length=10)
